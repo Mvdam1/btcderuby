ad024c98646bdb30ebf9fce3b9ae8b20af11bdf0
Matheus Vinicius de Alexandro Martins <145573301+Mvdam1@users.noreply.github.com>
2024-02-27 19:00:15 -0300

    Add files via upload

---
+++ projetodobootcamp.rb
@@ -0,0 +1,7 @@
+#projeto do curso de ruby, para a entrada e saida de dados.
+
+puts ("Qual seu nomew completo?")
+nome = gets.chomp
+puts ("Qual sua idade??")
+idade = gets.chomp.to_i
+puts ("Seu nome é #{nome} e voce tem #{idade} anos")
